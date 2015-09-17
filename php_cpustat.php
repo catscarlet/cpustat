ea37acc096787d0d0fc2c08fd333b5106d7fd743
catscarlet <i@catscarlet.com>
2015-09-17 16:03:03 +0800

    rewrite some logic

--- php_cpustat.php
+++ php_cpustat.php
@@ -2,40 +2,38 @@
 
 $procstatarray_s1 = getprocstat();
 
-foreach ($procstatarray_s1 as $cpuid => $procstat_s1) {
-    $totalCpuTime_s1[$cpuid] = array_sum($procstat_s1);
-    $idleCpuTime_s1[$cpuid] = $procstat_s1['idle'];
-}
-
 sleep(1);
 
 $procstatarray_s2 = getprocstat();
 
-foreach ($procstatarray_s2 as $cpuid => $procstat_s2) {
-    $totalCpuTime_s2[$cpuid] = array_sum($procstat_s2);
-    $idleCpuTime_s2[$cpuid] = $procstat_s2['idle'];
-}
+$nproc = getnproc();
 
-for ($cpuid = 0; $cpuid < count($procstatarray_s1); ++$cpuid) {
-    $totalCpuTime[$cpuid] = $totalCpuTime_s2[$cpuid] - $totalCpuTime_s1[$cpuid];
-    $idleCpuTime[$cpuid] = $idleCpuTime_s2[$cpuid] - $idleCpuTime_s1[$cpuid];
+for ($cpuid = 0; $cpuid < $nproc; ++$cpuid) {
+    $totalCpuTime[$cpuid] = $procstatarray_s2[$cpuid]['total'] - $procstatarray_s1[$cpuid]['total'];
+    $idleCpuTime[$cpuid] = $procstatarray_s2[$cpuid]['idle'] - $procstatarray_s1[$cpuid]['idle'];
     $pcpu[$cpuid] = 100 * ($totalCpuTime[$cpuid] - $idleCpuTime[$cpuid]) / $totalCpuTime[$cpuid];
     $pcpu[$cpuid] = round($pcpu[$cpuid], 2);
-    echo $pcpu[$cpuid].'%';
+    echo 'cpu'.$cpuid.' '.$pcpu[$cpuid].'%';
     echo "\n";
+}
+
+function getnproc()
+{
+    //get the number of processing units available
+    exec('nproc', $nproc);
+    $nproc = $nproc[0];
+
+    return $nproc;
 }
 
 function getprocstat()
 {
-    exec('nproc', $nproc);
-    $nproc = $nproc[0];
-    $time = time();
+    $nproc = getnproc();
+
     exec('cat /proc/stat|grep "^cpu"|tail -n '.$nproc, $procstat);
-    foreach ($procstat as $k => $v) {
+    foreach ($procstat as $cpuid => $v) {
         preg_match('/cpu(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)/', $v, $procstat_percpu);
-        $tmp[$k] = array(
-                //'time' => (int) $time,
-                //'cpu' => (int) $procstat_percpu[1],
+        $tmp[$cpuid] = array(
                 'user' => (int) $procstat_percpu[2],
                 'nice' => (int) $procstat_percpu[3],
                 'system' => (int) $procstat_percpu[4],
@@ -46,7 +44,11 @@ function getprocstat()
                 'steal' => (int) $procstat_percpu[9],
                 'guest' => (int) $procstat_percpu[10],
                 'guest_nice' => (int) $procstat_percpu[11],
-               );
+                );
+        $tmpsum = array_sum($tmp[$cpuid]);
+        $time = time();
+        $tmp[$cpuid]['total'] = $tmpsum;
+        $tmp[$cpuid]['time'] = $time;
     }
 
     return $tmp;
